e7709d953d3bd0437f0b3b22fe21a6f781db613b
luckgon <rm88349@fiap.com.br>
2023-08-22 00:32:13 -0300

    update

---
+++ cinestream/src/components/Card.jsx
@@ -0,0 +1,15 @@
+export default function Card () {
+    return(
+        <>
+            <div className=" w-40 h-64 rounded-lg bg-black">
+
+
+
+            </div>
+        
+        
+        </>
+
+    )
+
+}

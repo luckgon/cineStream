b60a650a2ea76c00f6686516367b1bca51fc5c24
luckgon <rm88349@fiap.com.br>
2023-08-22 00:42:24 -0300

    update card

--- cinestream/src/components/Card.jsx
+++ cinestream/src/components/Card.jsx
@@ -1,9 +1,14 @@
 export default function Card () {
     return(
         <>
-            <div className=" w-40 h-64 rounded-lg bg-black">
+            <div className=" w-40 h-64 rounded-lg bg-black flex justify-between p-4 ml-7 pt-48">
 
-
+                <svg className=" text-white w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                    <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z" clip-rule="evenodd" />
+                </svg>
+                <svg className =" text-white w-8"xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
 
             </div>
         

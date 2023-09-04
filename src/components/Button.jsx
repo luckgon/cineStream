ff0eeb17d3778c36ae266abce9eb1b3334e778d4
luckgon <rm88349@fiap.com.br>
2023-09-04 20:46:50 -0300

    Inclusão da pg de cadastro e componente button

---
+++ src/components/Button.jsx
@@ -0,0 +1,17 @@
+export default function Button () {
+    return(
+        <>
+            <div className=" w-48 h-72 min-w-[192px] rounded-lg bg-black flex justify-between ml-7  mb-3 ">
+
+                <a href=""></a>
+               
+
+
+            </div>
+            
+        
+        </>
+
+    )
+
+}

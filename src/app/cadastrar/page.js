bd3722d18a7c0044506f9497889e446d6ce22f42
luckgon <rm88349@fiap.com.br>
2023-09-04 21:45:17 -0300

    update

--- src/app/cadastrar/page.js
+++ src/app/cadastrar/page.js
@@ -1,6 +1,7 @@
 import NavBar from "@/components/NavBar"
 
 
+
 export default function Cadastrar (){
 
     return(
@@ -8,14 +9,11 @@ export default function Cadastrar (){
         <>
             <NavBar/>
             <main className=" pt-20 bg-gray-400 h-screen  text-violet-950 ">
-                {/* <h1>Cadastrar novo filme</h1> */}
+                
                 <div className=" bg-gray-300  ml-auto mr-auto w-auto max-w-[518px] h-auto rounded-md flex-row justify-center items-center p-7 shadow-xl" >
                     <h1 className=" flex justify-center text-4xl text-violet-950 font-bold mb-10">Cadastrar novo filme</h1>
                     <div className=" flex flex-col justify-center mt-7 items-center w-auto">
-                        {/* <div className=" flex-col justify-center items-center">
-                            <p className=" ">E-mail</p>
-                            <input className=" flex justify-center rounded-md p-2 w-96 h-8" type="text" />
-                        </div> */}
+                        
 
                         <div>
                             <p className="mt-4">Nome</p>
@@ -39,9 +37,23 @@ export default function Cadastrar (){
                         </div>
                         <div>
                             <p className="mt-4">Resumo</p>
-                            {/* <input className=" flex justify-center rounded-md p-2 mt-1 w-96 h-8" type="text" placeholder='' /> */}
+                            
                             <textarea className=" flex justify-center rounded-md p-2 mt-1 w-96 h-auto"></textarea>
 
+                        </div>
+                        <div className=" w-96 flex flex-row justify-between  mt-10 items-center">
+
+                            <div className=" flex">
+
+                                <button className=" bg-violet-700 w-24 h-8 rounded-md text-white" >Cadastrar</button>
+
+                            </div>
+
+                            <div className="flex">
+
+                                <button className=" w-24  h-8 rounded-md text-violet-700 border-solid border-2 border-violet-700" >Cancelar</button>
+
+                            </div>
                         </div>
                      
 
